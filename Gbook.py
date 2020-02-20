cb6e667f01e6631b9ab403011ca9f631c1b352ef
Simon Liu <simonliu07@gmail.com>
2020-02-20 11:48:49 -0800

    Update to create Gbook once

--- Gbook.py
+++ Gbook.py
@@ -7,29 +7,38 @@ class Gbooks:
     def __init__(self, days: int, num_lib:int ) -> int:
         self.days = days              #Save total time limit we have
         self.library = num_lib        #Total amount of libraries
-        self.stored_books = {}             #The books inside G Books
-        self.score = 0                #total scores this method got
+        self.stored_books ={}            #The books inside G Books
+        self.score = 0               #total scores this method got
 
     def computeScore(self, order, libraries) -> int:
+        self.score = 0
+        self.stored_books = {}
+        days_left = self.days
         #Go through the libaries in given order
         for lib_num in order:
-            self.days -= libraries[lib_num].signup_process  #days left after signup
+            days_left -= libraries[lib_num].signup_process  #days left after signup
             rate = libraries[lib_num].shipping_rate         #get the shipping rate of this library
-            if self.days <= 0:
+            #if there is no more days left to process after signup, quit the loop
+            if days_left <= 0:
                 break
+            #need to start compute the book sent by this library
             else:
-                day_counter = 0
+                day_counter = 0     #number of days we have shipped books
                 length = len(libraries[lib_num].book_ids)
-                count = 0
-                rate_count = 0
-                while(day_counter < self.days and count < length):
+                count = 0           #amount of books we have processed in this library
+                rate_count = 0      #books we have processed a day
+                #continue as long as we have days to process and there is book to ship
+                while(day_counter < days_left and count < length):
+                    #store the shipped books into a dictionary so we don't repeat
                     self.stored_books[libraries[lib_num].book_ids[count]] = libraries[lib_num].scores[count]
                     count += 1
                     rate_count += 1
+                    #if we reached max shipping per day, move one ot next day & reset rate_counter
                     if rate_count == rate:
                         day_counter +=1
                         rate_count = 0
 
+        #compute the score in Gbook for this method
         for key in self.stored_books:
             self.score += self.stored_books[key]
 
@@ -47,5 +56,4 @@ if __name__ == "__main__":
     G_book_1 = Gbooks(7,2)
     print(G_book_1.computeScore([0,1], World))
 
-    G_book_2 = Gbooks(7,2)
-    print(G_book_2.computeScore([1,0], World))
+    print(G_book_1.computeScore([1,0], World))
